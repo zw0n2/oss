2b6c6cc5623865676ec6997d3b447ab6df680484
ds-sujin <marcusss101@gmail.com>
2023-04-10 17:11:43 +0900

    modified

--- calc.py
+++ calc.py
@@ -3,12 +3,21 @@
 def plus(x, y):
     return x+y
 
+def sub(x,y):
+    return x-y
+
+def mul(x,y):
+    return x*y
+
+def div(x,y):
+    return x/y
+
 # main function
 def main():
     check = 1
     print("Welcome to calcuator")
     while check >= 1:        
-        print("0: exit, 1: plus")
+        print("0: exit, 1: plus, 2: sub, 3: mul, 4: div")
         check = int(input())
         if check == 1:
             print("First Number")
@@ -16,8 +25,24 @@ def main():
             print("Second Number")
             y = int(input())
             print("answer : ", plus(x,y))
-        elif check > 1:
-            print("Unsupported")
+        elif check == 2:
+            print("First Number")
+            x = int(input())
+            print("Second Number")
+            y = int(input())
+            print("answer : ", sub(x,y))
+        elif check == 3:
+            print("First Number")
+            x = int(input())
+            print("Second Number")
+            y = int(input())
+            print("answer : ", mul(x,y))
+        elif check == 4:
+            print("First Number")
+            x = int(input())
+            print("Second Number")
+            y = int(input())
+            print("answer : ", div(x,y))
         else:
             print("Thank you")
 
